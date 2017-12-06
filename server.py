3663cb6764f4c3b781d1126334510592ad875ce1
Georg Donner <georgdonner98@gmail.com>
2017-12-06 11:03:39 +0100

    Read room from query params and look up its occupation.

--- server.py
+++ server.py
@@ -1,8 +1,20 @@
-from flask import Flask, jsonify
+"""Web server which handles incoming requests to scrape room occupation from LSF"""
+
+import re
+from flask import Flask, request, jsonify
 import room_occupied
 APP = Flask(__name__)
 
 @APP.route('/')
-def getRoomOccupation():
-    occupied = room_occupied.main(['C', '335'])
-    return jsonify(occupied=occupied)
+def get_room_occupation():
+    """Parse room from query params and look up its occupation."""
+    room = request.args.get('room')
+    if room:
+        room_args = re.split('([0-9]+)', room)
+        if '' in room_args:
+            room_args.remove('')
+        if len(room_args) == 2:
+            occupied = room_occupied.main(room_args)
+            return jsonify(occupied=occupied)
+        return jsonify(error='Wrong room format. Room must have format C355.'), 400
+    return jsonify(error='Please provide a room in the query parameters.'), 400
